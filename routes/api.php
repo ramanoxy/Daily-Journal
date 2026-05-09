c8ae30c3ef10c4707f01fcec853481c0e60009d3
Alif Fathi <alif01fathi@gmail.com>
2026-05-09 19:46:55 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API Journal Core is online and ready.',
+        'timestamp' => now()->toIso8601String()
+    ], 200);
+});
